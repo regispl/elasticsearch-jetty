32e9d37d9d90ac83cf1553d7ad2dedfd1cf4a0c9
Michal Michalski <michal.michalski@boxever.com>
2014-02-14 13:15:26 +0000

    Rename package

--- utils/createpassword.sh
+++ utils/createpassword.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp elasticsearch-jetty-0.90.1-SNAPSHOT.jar:jetty-util-8.1.4.v20120524.jar:jasypt-1.9.1.jar com.sonian.elasticsearch.util.ExtendedPassword "$@"
+java -cp elasticsearch-jetty-0.90.1-SNAPSHOT.jar:jetty-util-8.1.4.v20120524.jar:jasypt-1.9.1.jar com.sonian.elasticsearch.security.ExtendedPassword "$@"
